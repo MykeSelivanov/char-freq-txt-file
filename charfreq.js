d42eab2dafe28f3908330ed2d8f9f857052f74f8
MykeSelivanov <michael777selivanov@gmail.com>
2021-08-08 22:37:27 -0500

    adding defaultmap and histogram classes

--- charfreq.js
+++ charfreq.js
@@ -1,5 +1,59 @@
 // DefaultMap extends Map so that the get() method returns the specified value instead of null
 // when key is not in the map
 class DefaultMap extends Map {
-    
+    constructor(defaultValue){
+        super();
+        this.defaultValue = defaultValue;
+    }
+
+    get(key){
+        // if the key is in the map, return it's value from superclass
+        if(this.has(key)) {
+            return super.get(key);
+        // otherwise default value will be returned
+        } else {
+            return this.defaultValue;
+        }
+    }
+}
+
+// Histogram class computes and displays letter frequency histograms
+class Histogram {
+    constructor(){
+        this.letterCounts = new DefaultMap(0);
+        this.totalLetters = 0;
+    }
+
+    // this functions updates the histogram with the letters of text
+    add(text) {
+        // removing whitespaces from the text and convert it to upper case
+        text = text.replace(/\s/g, "").toUpperCase();
+
+        // loop through the text characters
+        for (let char of text) {
+            let count = this.letterCounts.get(char);
+            this.letterCounts.set(character, count + 1);
+            this.totalLetters++;
+        }
+    }
+
+    // this func converts the histogram to a string that will display ASCII graphic
+    toString() {
+        // map to array of [k,v] arrays
+        let entries = [...this.letterCounts];
+
+        // sorting array: first by count and then alphabetically
+        entries.sort((a,b) => {
+            if (a[1] === b[1]) {
+                return a[0] < b[0] ? -1 : 1;
+            } else {
+                return b[1] - a[1];
+            }
+        });
+
+        // converting counts to %
+        for (let entry of entries) {
+            entry[1] = entry[1] / this.totalLetters * 100;
+        }
+    }
 }
