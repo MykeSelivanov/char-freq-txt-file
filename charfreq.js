3604230e8ec1697173cb04863bd00d0ad4f92eb8
MykeSelivanov <michael777selivanov@gmail.com>
2021-08-09 22:36:45 -0500

    formatting

--- charfreq.js
+++ charfreq.js
@@ -1,16 +1,16 @@
 // DefaultMap extends Map so that the get() method returns the specified value instead of null
 // when key is not in the map
 class DefaultMap extends Map {
-    constructor(defaultValue){
+    constructor(defaultValue) {
         super();
         this.defaultValue = defaultValue;
     }
 
-    get(key){
+    get(key) {
         // if the key is in the map, return it's value from superclass
-        if(this.has(key)) {
+        if (this.has(key)) {
             return super.get(key);
-        // otherwise default value will be returned
+            // otherwise default value will be returned
         } else {
             return this.defaultValue;
         }
@@ -19,7 +19,7 @@ class DefaultMap extends Map {
 
 // Histogram class computes and displays letter frequency histograms
 class Histogram {
-    constructor(){
+    constructor() {
         this.letterCounts = new DefaultMap(0);
         this.totalLetters = 0;
     }
@@ -43,7 +43,7 @@ class Histogram {
         let entries = [...this.letterCounts];
 
         // sorting array: first by count and then alphabetically
-        entries.sort((a,b) => {
+        entries.sort((a, b) => {
             if (a[1] === b[1]) {
                 return a[0] < b[0] ? -1 : 1;
             } else {
@@ -58,10 +58,10 @@ class Histogram {
 
         // disregard an entry if it is less than 1%
         entries = entries.filter(entry => entry[1] >= 1);
-    
+
         // converting each entry to the line of text
         let lines = entries.map(
-            ([l,n]) => `${l}: ${'#'.repeat(Math.round(n))} ${n.toFixed(2)}`
+            ([l, n]) => `${l}: ${'#'.repeat(Math.round(n))} ${n.toFixed(2)}`
         );
 
         // concatenate lines, separated with newline character
@@ -71,10 +71,10 @@ class Histogram {
 
 // This async function creates a Histogram object, asynchronosly reads chunks of text from standard input, and adds chunks 
 // to histogram. When end of the stream is reached, this histogram is returned
-async function histogramFromStdin(){
+async function histogramFromStdin() {
     process.stdin.setEncoding('utf-8'); // set to read unicode strings, not bytes
     let histogram = new Histogram();
-    for await (let chunk of process.stdin){
+    for await (let chunk of process.stdin) {
         histogram.add(chunk);
     }
     return histogram;
@@ -82,4 +82,4 @@ async function histogramFromStdin(){
 
 // Main body of the program
 // makes a Histogram object from stdin and then printts the histogram
-histogramFromStdin().then(histogram => {console.log(histogram.toString());});
+histogramFromStdin().then(histogram => { console.log(histogram.toString()); });
